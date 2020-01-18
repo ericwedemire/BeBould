4e6c842bc5bb526977c833c24ef4f23d8e9b04cc
petergryko <57876070+petergryko@users.noreply.github.com>
2020-01-18 12:50:50 -0700

    Update findRocks.py

--- findRocks.py
+++ findRocks.py
@@ -7,8 +7,33 @@ img = cv2.imread('../IMG.jpg')
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+#defining the Range of ??? color
 lower_range = np.array([110,50,50])
 upper_range = np.array([130,255,255])
+
+#defining the Range of Yellow color
+yellow_lower = np.array([0,0,0])
+yellow_upper = np.array([0,0,0])
+
+#defining the Range of Purple color
+purple_lower = np.array([0,0,0])
+purple_upper = np.array([0,0,0])
+
+#defining the Range of Green color
+green_lower = np.array([0,0,0])
+green_upper = np.array([0,0,0])
+
+#defining the Range of Red color
+red_lower = np.array([0,0,0])
+red_upper = np.array([0,0,0])
+
+#defining the Range of Black color
+black_lower = np.array([0,0,0])
+black_upper = np.array([0,0,0])
+
+#defining the Range of Blue color
+blue_lower = np.array([0,0,0])
+blue_upper = np.array([0,0,0])
 
 mask = cv2.inRange(hsv, lower_range, upper_range)
 
