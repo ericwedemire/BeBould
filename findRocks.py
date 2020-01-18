01fdaf927570b354e74c6c7f148c4aa82613e200
Mac Topp <macm.topp@gmail.com>
2020-01-18 16:55:44 -0700

    added boundries and changed color detection to all but grey

--- findRocks.py
+++ findRocks.py
@@ -3,19 +3,21 @@ import numpy as np
 import imutils
 import random as rng
 
-img = cv2.imread('RockPictures/20200116_144936.jpg')
+def mouse_drawing(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Left click", x, y)
+
+img = cv2.imread('RockPictures/IMG_20200116_143756.jpg')
 cv2.namedWindow("original", cv2.WINDOW_NORMAL)
 cv2.namedWindow("CV", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Edge", cv2.WINDOW_NORMAL)
-cv2.namedWindow("Boxes", cv2.WINDOW_NORMAL)
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # Accepted Colors
 #-----------
 #defining the Range of ??? color
-lower_range = np.array([110,50,50])
-upper_range = np.array([130,255,255])
+lower_range = np.array([25,25,25])
+upper_range = np.array([225,225,225])
 
 #defining the Range of Yellow color
 yellow_lower = np.array([212,192,121])
@@ -42,15 +44,12 @@ blue_upper = np.array([115,161,221])
 blue_lower = np.array([36,52,77])
 #-----------
 
-mask = cv2.inRange(hsv, blue_lower,blue_upper)
+mask = cv2.inRange(hsv, lower_range,upper_range)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edgeMap = imutils.auto_canny(mask)
 threshold = 100
 ret,thresh = cv2.threshold(mask,250,255,cv2.THRESH_BINARY_INV)
 contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 canny_output = cv2.Canny(mask, threshold, threshold * 2)
-# _, contours = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 contours_poly = [None]*len(contours)
 boundRect = [None]*len(contours)
@@ -67,21 +66,17 @@ hierarchy = hierarchy[0]
 for i in range(len(contours)):
     currentHierarchy = hierarchy[i]
     color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-    #cv2.drawContours(drawing, contours_poly, i, color)
-    
-    if int(radius[i]) > 12:
-        cv2.circle(drawing, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color, 2)
+    if int(radius[i]) > 10 and int(radius[i]) < 200:
+        cv2.circle(img, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color, 2)
+        # Add values to a list for collision detection
     
 
 imS = cv2.resize(img, (960, 540))
 imM = cv2.resize(mask, (960, 540))
-imE = cv2.resize(edgeMap, (960, 540))
-imB = cv2.resize(drawing, (960, 540))
 
 cv2.imshow('original', img)
 cv2.imshow('CV', mask)
-cv2.imshow('Edge', edgeMap)
-cv2.imshow('Boxes', drawing)
+cv2.setMouseCallback('original', mouse_drawing)
 
 while(True):
    k = cv2.waitKey(5) & 0xFF
