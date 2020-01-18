ca2d1e0cebe22890b08f53bc56efa1d4e7a7aab7
petergryko <57876070+petergryko@users.noreply.github.com>
2020-01-18 13:42:20 -0700

    Update findRocks.py

--- findRocks.py
+++ findRocks.py
@@ -8,47 +8,47 @@ img = cv2.imread('../IMG.jpg')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # Accepted Colors
-   #-----------
-   #defining the Range of ??? color
-   lower_range = np.array([110,50,50])
-   upper_range = np.array([130,255,255])
+#-----------
+#defining the Range of ??? color
+lower_range = np.array([110,50,50])
+upper_range = np.array([130,255,255])
 
-   #defining the Range of Yellow color
-   yellow_lower = np.array([212,192,121])
-   yellow_upper = np.array([130,102,28])
+#defining the Range of Yellow color
+yellow_lower = np.array([212,192,121])
+yellow_upper = np.array([130,102,28])
 
-   #defining the Range of Purple color
-   purple_lower = np.array([125,104,161])
-   purple_upper = np.array([49,39,63])
+#defining the Range of Purple color
+purple_lower = np.array([125,104,161])
+purple_upper = np.array([49,39,63])
 
-   #defining the Range of Green color
-   green_lower = np.array([49,96,62])
-   green_upper = np.array([23,48,29])
+#defining the Range of Green color
+green_lower = np.array([49,96,62])
+green_upper = np.array([23,48,29])
 
-   #defining the Range of Red color
-   red_lower = np.array([200,94,96])
-   red_upper = np.array([51,22,26])
+#defining the Range of Red color
+red_lower = np.array([200,94,96])
+red_upper = np.array([51,22,26])
 
-   #defining the Range of Black color
-   black_lower = np.array([97,102,121])
-   black_upper = np.array([38,38,38])
+#defining the Range of Black color
+black_lower = np.array([97,102,121])
+black_upper = np.array([38,38,38])
 
-   #defining the Range of Blue color
-   blue_lower = np.array([115,161,221])
-   blue_upper = np.array([36,52,77])
-   #-----------
+#defining the Range of Blue color
+blue_lower = np.array([115,161,221])
+blue_upper = np.array([36,52,77])
+#-----------
 
 # Background Colors To Avoid (Gray/Pink wall)
-   #-----------
+#-----------
 
-   #defining the Range of Gray Wall
-   gray_lower = np.array([183,179,178])
-   gray_upper = np.array([103,102,100])
+#defining the Range of Gray Wall
+gray_lower = np.array([183,179,178])
+gray_upper = np.array([103,102,100])
 
-   #defining the Range of Pink Wall
-   pink_lower = np.array([223,164,196])
-   pink_upper = np.array([162,177,110])
-   #-----------
+#defining the Range of Pink Wall
+pink_lower = np.array([223,164,196])
+pink_upper = np.array([162,177,110])
+#-----------
 
 mask = cv2.inRange(hsv, lower_range, upper_range)
 
