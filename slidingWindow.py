37568edb7231d84964ed4df8d30c65137c4b7790
Michael Kaye <kayem6@mymacewan.ca>
2020-03-25 15:02:05 -0600

    Add sliding window program

---
+++ slidingWindow.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import imutils
+import argparse
+import time
+import cv2
+
+
+def pyramid(image, scale=1.5, minSize=(30, 30)):
+	# yield the original image
+	yield image
+	# keep looping over the pyramid
+	while True:
+		# compute the new dimensions of the image and resize it
+		w = int(image.shape[1] / scale)
+		image = imutils.resize(image, width=w)
+		# if the resized image does not meet the supplied minimum
+		# size, then stop constructing the pyramid
+		if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+			break
+		# yield the next image in the pyramid
+		yield image
+
+
+def sliding_window(image, stepSize, windowSize):
+	# slide a window across the image
+	for y in range(0, image.shape[0], stepSize):
+		for x in range(0, image.shape[1], stepSize):
+			# yield the current window
+			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+
+
+def main():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the image")
+    args = vars(ap.parse_args())
+    # load the image and define the window width and height
+    image = cv2.imread(args["image"])
+
+    # resize image
+    scale_percent = 25
+    #calculate the 50 percent of original dimensions
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
+    # dsize
+    dsize = (width, height)
+    # resize image
+    image = cv2.resize(image, dsize)
+
+    (winW, winH) = (64, 64)
+
+    # loop over the image pyramid
+    for resized in pyramid(image, scale=1.5):
+
+        # loop over the sliding window for each layer of the pyramid
+        for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
+
+            # if the window does not meet our desired window size, ignore it
+            if window.shape[0] != winH or window.shape[1] != winW:
+                continue
+
+            # -------------------------------------------------------------------------------------------
+            # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+            # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+            # WINDOW
+            # since we do not have a classifier, we'll just draw the window
+            # -------------------------------------------------------------------------------------------
+
+            clone = resized.copy()
+            cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+            cv2.imshow("Window", clone)
+            cv2.waitKey(1)
+            time.sleep(0.025)
+
+if __name__ == "__main__":
+   main()
